51b45008b9bd1b4863f7234b07a4a010d00a8701
Plata <pl4t417@gmail.com>
2016-12-01 01:20:47 +0100

    First commit with test code and gradle. Hello Pi!

---
+++ src/main/java/Main.java
@@ -0,0 +1,11 @@
+package main.java;
+
+/**
+ * Created by Plata on 30/11/2016.
+ */
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World on Raspberry PI!");
+    }
+}
